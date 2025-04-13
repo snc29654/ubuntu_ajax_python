2dd68d7876b88f4e508a818a14e012965d9b709f
snc29654 <snc29654@nifty.com>
2025-04-13 11:11:23 +0900

    ai chat履歴の質問文字を赤とする

--- ai_chat.py
+++ ai_chat.py
@@ -76,6 +76,7 @@ with closing(sqlite3.connect(dbname)) as conn:
     Contents = str(scraping_contents)
     Contents = Contents.replace ("\n","")
     Contents = Contents.replace ("\t","")
+    memo_title="<font color=\"red\">"  + memo_title + "</font>"
     insert_sql = 'insert into users (date, name, weather, kind, zip_code,Contents) values (?,?,?,?,?,?)'
     users = [
     (date, name, weather, kind, memo_title,Contents)
