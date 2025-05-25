91e09510cc96b2a86e079dec17ce694c764e3f06
snc29654 <snc29654@nifty.com>
2025-05-25 12:49:19 +0900

    カテゴリ別履歴　URLリストでurlでクリック可能とする

--- inet_list.py
+++ inet_list.py
@@ -115,6 +115,7 @@ with closing(sqlite3.connect(dbname)) as conn:
     Contents = str(scraping_contents)
     Contents = Contents.replace("\n","")
     Contents = Contents.replace("\t","")
+    zip_code="<a href=\""+zip_code+"\" target=\"_blank\">"+zip_code+"</a>"
     insert_sql = 'insert into users (date, name, weather, kind, zip_code,Contents) values (?,?,?,?,?,?)'
     users = [
     (date, name, weather, kind, zip_code,Contents)
